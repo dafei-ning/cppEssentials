0f9babdc99975343be4e21f9210a1f050fb13231
Dafei Ning <ningdafei@gmail.com>
2020-02-27 18:31:36 -0800

    format space in angles

--- 3.standardTemplateLibray/twoDimensionalVectors.cpp
+++ 3.standardTemplateLibray/twoDimensionalVectors.cpp
@@ -7,7 +7,7 @@ int main() {
     cout << endl;
 
     //创建 2 维grid，3个row，4个column，起始值为 5.
-    vector<vector<int>> grid(3, vector<int>(4, 5));
+    vector< vector< int > > grid(3, vector< int >(4, 5));
 
     for (int i = 0; i < grid.size(); i++) {
         for (int j = 0; j < grid[i].size(); j++) {
