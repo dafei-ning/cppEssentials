c35a9f9bd27d5503a046190ab92b009a4036a18e
jackningdl <ningdafei@gmail.com>
2020-04-05 19:53:00 -0700

    test3

--- 3.standardTemplateLibray/1.vectors.cpp
+++ 3.standardTemplateLibray/1.vectors.cpp
@@ -29,7 +29,7 @@ int main() {
     cout << endl;
 
     // 创建一个iterator，strings.begin()就是一个iterator
-    // another test 1
+    // another test 1 test 2
     vector<string>::iterator it;
     cout << "iterator goes: " << endl;
 
